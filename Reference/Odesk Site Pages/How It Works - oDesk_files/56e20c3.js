5e777646a11ca0f3720f7e825715ef73d5456ee4
feroj <feroj21@gmail.com>
2015-02-08 18:35:35 +0600

    15 No. page done.

--- Reference/Odesk Site Pages/How It Works - oDesk_files/56e20c3.js
+++ Reference/Odesk Site Pages/How It Works - oDesk_files/56e20c3.js
@@ -1,1 +1,41 @@
-(function($){"use strict";$(function(){var $window=$(window);var needHelp=$(".oStickyNotice"),needHelpHeight=needHelp.outerHeight(),stopTop=300,offsetTop=0,stopBottom=0;$window.on("resize load",function(){offsetTop=needHelp.position().top;stopBottom=$(document).height()-needHelpHeight-400});$window.on("scroll.showNeedHelp load.showNeedHelp",function(){if($window.width()<1280){return}var windowTop=$window.scrollTop(),positionTop=offsetTop+30;if(windowTop>=stopTop-120&&windowTop+offsetTop<stopBottom){needHelp.css({position:"fixed",top:""}).removeClass("affixTop").fadeIn(600)}else if(windowTop+offsetTop>=stopBottom){needHelp.css({position:"absolute",top:stopBottom})}});needHelp.find(".close").on("click",function(e){e.preventDefault();var width=needHelp.outerWidth();needHelp.animate({left:-width});$window.off("scroll.showNeedHelp load.showNeedHelp")})})})(jQuery);
+(function ($) {
+    "use strict";
+    $(function () {
+        var $window = $(window);
+        var needHelp = $(".oStickyNotice"),
+            needHelpHeight = needHelp.outerHeight(),
+            stopTop = 300,
+            offsetTop = 0,
+            stopBottom = 0;
+        $window.on("resize load", function () {
+            offsetTop = needHelp.position().top;
+            stopBottom = $(document).height() - needHelpHeight - 400
+        });
+        $window.on("scroll.showNeedHelp load.showNeedHelp", function () {
+            if ($window.width() < 1280) {
+                return
+            }
+            var windowTop = $window.scrollTop(),
+                positionTop = offsetTop + 30;
+            if (windowTop >= stopTop - 120 && windowTop + offsetTop < stopBottom) {
+                needHelp.css({
+                    position: "fixed",
+                    top: ""
+                }).removeClass("affixTop").fadeIn(600)
+            } else if (windowTop + offsetTop >= stopBottom) {
+                needHelp.css({
+                    position: "absolute",
+                    top: stopBottom
+                })
+            }
+        });
+        needHelp.find(".close").on("click", function (e) {
+            e.preventDefault();
+            var width = needHelp.outerWidth();
+            needHelp.animate({
+                left: -width
+            });
+            $window.off("scroll.showNeedHelp load.showNeedHelp")
+        })
+    })
+})(jQuery);
--- Reference/Odesk Site Pages/How It Works - oDesk_files/6795e76.js
+++ Reference/Odesk Site Pages/How It Works - oDesk_files/6795e76.js
@@ -1,1 +1,45 @@
-(function(){"use strict";(function($){$.fn.oStickyHeader=function(options){var settings=$.extend({onToggle:function(){}},options);var stickyHeader=this,pivot=$(stickyHeader.data("pivot")),$window=$(window),init=function(){$window.on("scroll",toggle);$window.on("resize",function(){if($window.width()<640){$window.off("scroll",toggle)}else{$window.on("scroll",toggle)}})},toggle=function(){stickyHeader.removeClass("visible hidden");if($window.scrollTop()>getOffsetTop()){"function"===typeof settings.onToggle&&settings.onToggle(true);stickyHeader.addClass("slide-down").slideDown(300,function(){$(this).removeClass("slide-down").addClass("visible")})}else{"function"===typeof settings.onToggle&&settings.onToggle(false);stickyHeader.addClass("slide-up").slideUp(300,function(){$(this).removeClass("slide-up").addClass("hidden")})}},getOffsetTop=function(){return pivot.offset().top};init();return this}})(jQuery);jQuery(function($){$(".oStickyHeader").oStickyHeader()})}).call(this);
+(function () {
+    "use strict";
+    (function ($) {
+        $.fn.oStickyHeader = function (options) {
+            var settings = $.extend({
+                onToggle: function () {}
+            }, options);
+            var stickyHeader = this,
+                pivot = $(stickyHeader.data("pivot")),
+                $window = $(window),
+                init = function () {
+                    $window.on("scroll", toggle);
+                    $window.on("resize", function () {
+                        if ($window.width() < 640) {
+                            $window.off("scroll", toggle)
+                        } else {
+                            $window.on("scroll", toggle)
+                        }
+                    })
+                },
+                toggle = function () {
+                    stickyHeader.removeClass("visible hidden");
+                    if ($window.scrollTop() > getOffsetTop()) {
+                        "function" === typeof settings.onToggle && settings.onToggle(true);
+                        stickyHeader.addClass("slide-down").slideDown(300, function () {
+                            $(this).removeClass("slide-down").addClass("visible")
+                        })
+                    } else {
+                        "function" === typeof settings.onToggle && settings.onToggle(false);
+                        stickyHeader.addClass("slide-up").slideUp(300, function () {
+                            $(this).removeClass("slide-up").addClass("hidden")
+                        })
+                    }
+                },
+                getOffsetTop = function () {
+                    return pivot.offset().top
+                };
+            init();
+            return this
+        }
+    })(jQuery);
+    jQuery(function ($) {
+        $(".oStickyHeader").oStickyHeader()
+    })
+}).call(this);
