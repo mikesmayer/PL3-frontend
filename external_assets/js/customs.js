1e8c266b55d7f070b6f8a6f3658f957222dfed4e
feroj <feroj21@gmail.com>
2015-03-04 23:37:00 +0600

    Index

--- external_assets/js/customs.js
+++ external_assets/js/customs.js
@@ -12,3 +12,25 @@ $(document.body).on('click', '.dropdown-menu li', function (event) {
     return false;
 
 });
+
+jQuery(document).ready(function () {
+
+//    jQuery("span.herolight").html();
+    value.forEach(function (entry) {
+        setTimeout(function () {
+            jQuery("span.herolight").html(entry)
+        }, 2000);
+    });
+
+//    setTimeout( "jQuery('#div').hide();",3000 );
+});
+var value = ['GLOBAL', 'LOCAL', 'REFERRAL', 'SALES', 'OVERSEAS', 'RESELLER', 'VAR', 'TECHNOLOGY', 'CLOUD', 'SAAS'];
+var counter = 0;
+setInterval(change, 3000);
+function change() {
+    jQuery("span.herolight").html(value[counter]);
+    counter++;
+    if (counter >= value.length) {
+        counter = 0;
+    }
+}
